f1c9f07276ad98a6fb19c6f2d5f22feec61d2974
kui66de <37660690+kui66de@users.noreply.github.com>
2019-02-17 19:25:51 +0000

    i add text to this page

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Playerunknow's BattleGrounds</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" type="image/png" href="images/icon.png">
+        <link rel="stylesheet" href="css/main.css"/>
+    </head>
+    <body>
+        
+    
+    
