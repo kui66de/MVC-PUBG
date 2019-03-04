6eecf662836223e2930fda63ff2bd80dc98be71a
shiku <shiku@10.102.23.45>
2019-03-04 17:18:33 +0000

    add user system

---
+++ users/welcome.php
@@ -0,0 +1,25 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<?php include '../view/header.php'; ?>
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["userName"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+<?php include 'view/footer.php'; ?>
