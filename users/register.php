cb95700e46dcd9ef8341a2b5df9f1a31731d3578
shiku <shiku@10.102.23.45>
2019-03-04 18:43:03 +0000

    last eddit

--- users/register.php
+++ users/register.php
@@ -1,6 +1,6 @@
 <?php
 // Include config file
-require_once "database.php";
+require_once('../database/database.php');
  
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
