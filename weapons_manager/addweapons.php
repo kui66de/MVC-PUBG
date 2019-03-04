df1bea7dc17829e31039599ade8a49520a2388da
shiku <shiku@10.102.23.45>
2019-03-04 09:50:47 +0000

    change form of addpage

--- weapons_manager/addweapons.php
+++ weapons_manager/addweapons.php
@@ -1,8 +1,38 @@
-<?php
+<?php include '../view/header.php'; ?>
+<main>
+    <h1>Add Weapon</h1>
+    <form action="index.php" method="post" id="add_weapon_form">
+        <input type="hidden" name="action" value="add_weapon">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <label>Category:</label>
+        <select name="category_id">
+        <?php foreach ( $categories as $category ) : ?>
+            <option value="<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        <br>
 
+        <label>Name:</label>
+        <input type="input" name="name">
+        <br>
+
+        <label>Ammo:</label>
+        <input type="input" name="ammo">
+        <br>
+
+        <label>Dmg:</label>
+        <input type="input" name="dmg">
+        <br>
+
+        <label>&nbsp;</label>
+        <input type="submit" value="Add Weapon">
+        <br>
+    </form>
+    <p class="last_paragraph">
+        <a href="index.php?action=list_weapon">View Weapon List</a>
+    </p>
+
+</main>
+<?php include '../view/footer.php'; ?>
